19fd694a701b0303426e1f876a3daeb7efbca0de
tianopo <matheuslink18@hotmail.com>
2023-12-02 12:05:24 -0300

    configurações de projetos pronto para rodar

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RotasApp } from './rotas/rotasApp';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-
-        <RotasApp />
-
+      <RotasApp />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
