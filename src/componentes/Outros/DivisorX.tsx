6525e328723625d2a785999e4dd67b07ad7c1c98
tianopo <matheuslink18@hotmail.com>
2023-12-05 18:20:53 -0300

    divisor de tela

---
+++ src/componentes/Outros/DivisorX.tsx
@@ -0,0 +1,3 @@
+export const DivisorX = () => {
+  return <hr className="w-full border-1 border-borda"></hr>
+} 
--- src/paginas/teste.tsx
+++ src/paginas/teste.tsx
@@ -1,12 +1,14 @@
 import { BotaoOnClick } from "src/componentes/Botoes/BotaoOnClick";
 import { BotaoSubmit } from "src/componentes/Botoes/BotaoSubmit";
 import { FlexCol } from "src/componentes/Flex/FlexCol";
+import { DivisorX } from "src/componentes/Outros/DivisorX";
 
 export const Teste = () => {
   return (
     <FlexCol className={'bg-escrita'}>
       <BotaoSubmit> Enviar </BotaoSubmit>
       <BotaoOnClick onClick={() => "oi"}> Gosto </BotaoOnClick>
+      <DivisorX />
     </FlexCol>
   );
 };
