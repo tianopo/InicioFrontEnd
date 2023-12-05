474cec9aee7ccd594601f5407f08cfef38c77073
tianopo <matheuslink18@hotmail.com>
2023-12-05 19:06:56 -0300

    tipos de texto

---
+++ src/componentes/Outros/TextoX.tsx
@@ -0,0 +1,10 @@
+import React, { HTMLAttributes } from "react";
+
+interface ITextoProps extends HTMLAttributes<HTMLDivElement> {
+  tipo: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "pre" | "i" | "u" | "strong" | "em" | "blockquote" | "cite" | "code" | "q" | "abbr";
+  children?: React.ReactNode;
+}
+
+export const TextoX = ({ tipo, children, className, ...rest }: ITextoProps) => {
+  return React.createElement(tipo, { className, ...rest }, children);
+};
--- src/paginas/teste.tsx
+++ src/paginas/teste.tsx
@@ -3,14 +3,19 @@ import { BotaoSubmit } from "src/componentes/Botoes/BotaoSubmit";
 import { FlexCol } from "src/componentes/Flex/FlexCol";
 import { DivisorX } from "src/componentes/Outros/DivisorX";
 import { LinkX } from "src/componentes/Outros/LinkX";
+import { TextoX } from "src/componentes/Outros/TextoX";
 
 export const Teste = () => {
   return (
-    <FlexCol className={'bg-escrita'}>
+    <FlexCol className={"bg-escrita"}>
       <BotaoSubmit> Enviar </BotaoSubmit>
       <BotaoOnClick onClick={() => "oi"}> Gosto </BotaoOnClick>
       <DivisorX />
-      <LinkX rota="./home"><h1>olá</h1></LinkX>
+      <LinkX rota="./home">
+        <h1>olá</h1>
+      </LinkX>
+      <TextoX tipo="i">tudo bem</TextoX>
+      <TextoX tipo="h6">tudo bem</TextoX>
     </FlexCol>
   );
 };
