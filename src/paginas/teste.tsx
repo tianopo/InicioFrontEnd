4c43d2e1ea6461090d1beec9647320785967ed7b
tianopo <matheuslink18@hotmail.com>
2023-12-18 09:26:45 -0300

    personalização de botao

--- src/paginas/teste.tsx
+++ src/paginas/teste.tsx
@@ -88,6 +88,8 @@ export const Teste = () => {
 };
 // implementar temas de escuro e claro
 // configurar cores do projeto
+// Botão de troca de tema inicial aparece vazio
+// ver bug de trocar idioma
 // separação de inputs para implementar label, inputs e mensagem de erros em uma só
 // carregamento de componentes para performance como do facebook
 // separação de display inline e display block
