7590277b5ced5b656c2e326a3790ff60f47de7d8
tianopo <matheuslink18@hotmail.com>
2023-12-18 09:37:41 -0300

    cores de outros ajustado

--- src/componentes/Outros/DivisorX.tsx
+++ src/componentes/Outros/DivisorX.tsx
@@ -1,3 +1,7 @@
+import { useTema } from "src/hooks/configuracao/useTema";
+
 export const DivisorX = () => {
-  return <hr className="w-full border-1 border-borda"></hr>;
+  const { tema } = useTema();
+
+  return <hr className={`w-full border-1 divisor-${tema}`} ></hr >;
 };
--- src/componentes/Outros/LinkX.tsx
+++ src/componentes/Outros/LinkX.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useTema } from "src/hooks/configuracao/useTema";
 
 export interface ILinkX {
   children?: React.ReactNode;
@@ -6,12 +7,14 @@ export interface ILinkX {
   target?: string;
 }
 
-export const LinkX = ({ rota, children, target }: ILinkX) => (
-  <Link
+export const LinkX = ({ rota, children, target }: ILinkX) => {
+  const { tema } = useTema();
+
+  return <Link
     to={rota!}
     target={target}
-    className="w-fit font-normal text-primaria transition duration-300 ease-in-out hover:opacity-80"
+    className={`w-fit font-normal transition duration-300 ease-in-out hover:opacity-80 link-${tema}`}
   >
     {children}
   </Link>
-);
+}
