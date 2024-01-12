b5b04c554aaba3e3ac5924230917922c5c85b216
tianopo <matheuslink18@hotmail.com>
2024-01-12 11:46:57 -0300

    começando as traduções em 9 linguas

--- src/componentes/Botoes/BotaoTrocarIdioma.tsx
+++ src/componentes/Botoes/BotaoTrocarIdioma.tsx
@@ -16,12 +16,36 @@ export const BotaoTrocarIdioma = ({ menuBottom }: IBotaoTrocarIdioma) => {
       flag: "/flags/br.svg",
     },
     {
-      valor: "en",
-      flag: "/flags/us.svg",
+      valor: "cn",
+      flag: "/flags/cn.svg",
+    },
+    {
+      valor: "de",
+      flag: "/flags/de.svg",
+    },
+    {
+      valor: "dr",
+      flag: "/flags/dr.svg",
+    },
+    {
+      valor: "es",
+      flag: "/flags/es.svg",
     },
     {
       valor: "fr",
       flag: "/flags/fr.svg",
+    },
+    {
+      valor: "it",
+      flag: "/flags/it.svg",
+    },
+    {
+      valor: "jp",
+      flag: "/flags/jp.svg",
+    },
+    {
+      valor: "us",
+      flag: "/flags/us.svg",
     },
   ];
 
--- src/idiomas/tradutor.ts
+++ src/idiomas/tradutor.ts
@@ -1,18 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enJson from "./traducoes/en.json";
-import frJson from "./traducoes/fr.json";
+import alemaoJson from "./traducoes/alemao.json";
+import chinesJson from "./traducoes/chines.json";
+import draconicoJson from "./traducoes/draconico.json";
+import espanholJson from "./traducoes/espanhol.json";
+import francesJson from "./traducoes/frances.json";
+import inglesJson from "./traducoes/ingles.json";
+import italianoJson from "./traducoes/italiano.json";
+import japonesJson from "./traducoes/japones.json";
 import ptbrJson from "./traducoes/ptbr.json";
 
 i18n.use(initReactI18next).init({
-  fallbackLng: "en",
+  fallbackLng: "us",
   interpolation: {
     escapeValue: false,
   },
   resources: {
-    en: enJson,
-    fr: frJson,
+    us: inglesJson,
+    fr: francesJson,
     ptbr: ptbrJson,
+    de: alemaoJson,
+    cn: chinesJson,
+    dr: draconicoJson,
+    es: espanholJson,
+    it: italianoJson,
+    jp: japonesJson,
   },
 });
 
