f3f1d4518b8293a2c21d712857188e61bec0b4cd
tianopo <matheuslink18@hotmail.com>
2024-01-16 17:33:25 -0300

    sidebar pronta

--- src/componentes/Layout/Sidebar.tsx
+++ src/componentes/Layout/Sidebar.tsx
@@ -41,7 +41,6 @@ export const Sidebar = ({ imagem, titulo, navbar, idioma, temaCor, sair }: ISide
       rounded-r-6
       border-1
       p-2
-      md:hidden
       `}
       >
         <button
@@ -66,16 +65,14 @@ export const Sidebar = ({ imagem, titulo, navbar, idioma, temaCor, sair }: ISide
       border-1
       p-4
       md:sticky
-      md:flex
+      ${menuAberto ? "md:flex" : "hidden"}
       md:w-1/3
       `}
       >
-        {menuAberto && (
-          <X
-            className="h-40 w-10 cursor-pointer self-end text-white"
-            onClick={() => setMenuAberto(!menuAberto)}
-          />
-        )}
+        <X
+          className="h-40 w-10 md:h-5 md:right-0 md:absolute cursor-pointer self-end text-white"
+          onClick={() => setMenuAberto(!menuAberto)}
+        />
         {imagem && <img src={imagem} alt={titulo} className={`h-20 w-20`} />}
         {titulo && (
           <TX tipo="h2" className="text-center text-2xl font-bold text-white">
